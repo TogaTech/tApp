0d168c9878a662f431bcf56a386dca12991c357b
CMEONE <contact@kabirwearsato.ga>
2021-03-03 22:19:23 -0800

    Add example

---
+++ example/config.js
@@ -0,0 +1,44 @@
+tApp.config = {
+	target: document.querySelector("tapp-main"),
+	ignoreRoutes: ["#id"],
+	forbiddenRoutes: ["#/forbidden"],
+	errorPages: {
+		notFound: "#/404",
+		forbidden: "#/403"
+	}
+};
+
+tApp.route("/", function(request) {
+	tApp.redirect("#/");
+});
+
+tApp.route("#/", function(request) {
+	tApp.renderFile("./views/index.html");
+});
+
+tApp.route("#/about", function(request) {
+	tApp.renderFile("./views/about.html");
+});
+
+tApp.route("#/text", function(request) {
+	tApp.render(`
+		<h1>Text</h1>
+		<p>You can place any text here.</p>
+	`);
+});
+
+tApp.route("#/404", function(request) {
+	tApp.render(`
+		<h1>Error 404</h1>
+		<p>Page not found.</p>
+	`);
+});
+
+tApp.route("#/403", function(request) {
+	tApp.render(`
+		<h1>Error 403</h1>
+		<p>Access denied.</p>
+	`);
+});
+
+tApp.start();
